809a6467fafc6331d094c56941e22e728ab64ec5
BlueEeyore <ronnie.livne@gmail.com>
2025-04-05 13:05:48 +1300

    improved transcription module to be a class and to use temp files. Still need to do error handling

--- transcription.py
+++ transcription.py
@@ -1,28 +1,50 @@
 import whisper
 from moviepy import VideoFileClip
+from tempfile import NamedTemporaryFile
 
 
 AUDIO_FILENAME = "audio_file.wav"
 WHISPER_MODEL = "tiny"
 
 
-def trans_audio(audio_file):
-    """takes audio file as input and outputs transcription"""
-    model = whisper.load_model(WHISPER_MODEL)
-    result = model.transcribe(audio_file, fp16=False)
-    return result["text"]
+class Transcription:
+    def __init__(self, model=WHISPER_MODEL):
+        "initialising whisper model to be used"
+        self.model_str = whisper_model
+        self.model = whisper.load_model(whisper_model)
+        
+    def trans_audio(self, audio_file):
+        """takes audio file as input and outputs transcription"""
+        result = self.model.transcribe(audio_file, fp16=False)
+        return result
 
+    def trans_video(self, video_file):
+        """takes video file as input and outputs audio transcription dict"""
+        # set up a temporary audio file
+        temp_audio_file = NamedTemporaryFile(suffix=".mp3")
 
-def trans_video(video_file):
-    """takes video file as input and outputs audio transcription"""
-    video = VideoFileClip(video_file)
-    audio = video.audio
-    audio.write_audiofile(AUDIO_FILENAME)
+        # open video file and write audio to temp file
+        video = VideoFileClip(video_file)
+        audio = video.audio
+        audio.write_audiofile(temp_audio_file.name)
 
-    result = trans_audio(AUDIO_FILENAME)
+        # close files
+        video.close()
+        audio.close()
 
-    return result
+        # transcribe the audio
+        result = self.trans_audio(temp_audio_file.name)
 
+        return result
+    
 
-# print(trans_audio("harvard.wav"))
-print(trans_video("sample_video.mp4"))
+if __name__ == "__main__":
+    transcriber = Transcription()
+
+    result_dict = transcriber.trans_audio("harvard.wav")
+    segments = [{"id":x["id"],"start":x["start"],"end":x["end"],"text":x["text"]} for x in result_dict["segments"]]
+    print(segments)
+
+    result_dict = transcriber.trans_video("sample_video.mp4")
+    print(result_dict)
+
