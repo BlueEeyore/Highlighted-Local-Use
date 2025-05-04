e08f858ed9c556e1e920bff7f75eadba453ddb1a
BlueEeyore <ronnie.livne@gmail.com>
2025-05-05 10:22:07 +1200

    seemed to be an issue with the session_globals module in the previous commit. This is more up to date (changed from using a class to using functions)

--- main/app/session_globals.py
+++ main/app/session_globals.py
@@ -1,47 +1,118 @@
 import logging
+from flask import session
+from datetime import timedelta
 logger = logging.getLogger(__name__)
 
 
-class SessionGlobals:
-    def __init__(self):
-        logger.debug("initialising")
 
-        self.vars = {}
 
-    def set(self, key, value):
-        """assigns key to value"""
-        logger.debug(f"setting {key} to {value}")
+def session_config(app):
+    """configures session"""
+    app.secret_key = "very-secret-key"
+    app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=30)
 
-        self.vars[key] = value
 
-    def get(self, key):
-        """gets value associated with given key"""
-        logger.debug(f"getting {key}")
-        
-        return self.vars[key]
+def set(key, value):
+    """assigns key to value"""
+    logger.debug(f"setting {key} to {value}")
 
-    def remove(self, key):
-        """removes item associated with given key"""
-        logger.debug(f"removing {key}")
+    # checking if the global dictionary has been created yet. If not, creates it
+    if "global_dict" not in session:
+        logger.debug("creating global_dict")
+        session["global_dict"] = {}
 
-        del self.vars[key]
+    session["global_dict"][key] = value
 
-    def increment(self, key):
-        """increments value associated with given key (must be int)"""
-        logger.debug(f"incrementing {key}")
 
-        try:
-            self.vars[key] += 1
-        except TypeError as e:
-            logger.exception(e)
-            raise
+def get(key):
+    """gets value associated with given key"""
+    logger.debug(f"getting {key}")
 
-    def decrement(self, key):
-        """decrements value associated with given key (must be int)"""
-        logger.debug(f"decrementing {key}")
+    if key in session["global_dict"]:
+        return session["global_dict"][key]
 
-        try:
-            self.vars[key] -= 1
-        except TypeError as e:
-            logger.exception(e)
-            raise
+    logger.error(f"{key} not found in dictionary")
+    ErrorStack.push(f"{key} not in dictionary")
+    return None
+
+
+def remove(key):
+    """removes item associated with given key"""
+    logger.debug(f"removing {key}")
+    del session["global_dict"][key]
+    
+
+def print_dict():
+    """prints the global dictionary (used only for debugging)"""
+    logger.debug(f"printing dictionary")
+    print(session["global_dict"])
+
+
+def increment(key):
+    """increments value associated with given key (must be int)"""
+    logger.debug(f"incrementing {key}")
+
+    try:
+        session["global_dict"][key] += 1
+    except TypeError as e:
+        logger.exception(e)
+        return None     # do this properly
+
+
+def decrement(key):
+    """decrements value associated with given key (must be int)"""
+    logger.debug(f"decrementing {key}")
+
+    try:
+        session["global_dict"][key] -= 1
+    except TypeError as e:
+        logger.exception(e)
+        raise
+
+
+
+
+
+# class SessionGlobals:
+#     def __init__(self):
+#         logger.debug("initialising")
+# 
+#         self.vars = {}
+# 
+#     def set(self, key, value):
+#         """assigns key to value"""
+#         logger.debug(f"setting {key} to {value}")
+# 
+#         self.vars[key] = value
+# 
+#     def get(self, key):
+#         """gets value associated with given key"""
+#         logger.debug(f"getting {key}")
+#         
+#         return self.vars[key]
+# 
+#     def remove(self, key):
+#         """removes item associated with given key"""
+#         logger.debug(f"removing {key}")
+# 
+#         del self.vars[key]
+# 
+#     def increment(self, key):
+#         """increments value associated with given key (must be int)"""
+#         logger.debug(f"incrementing {key}")
+# 
+#         try:
+#             self.vars[key] += 1
+#         except TypeError as e:
+#             logger.exception(e)
+#             raise
+# 
+#     def decrement(self, key):
+#         """decrements value associated with given key (must be int)"""
+#         logger.debug(f"decrementing {key}")
+# 
+#         try:
+#             self.vars[key] -= 1
+#         except TypeError as e:
+#             logger.exception(e)
+#             raise
--- main/testing_database.py
+++ main/testing_database.py
@@ -3,4 +3,5 @@ from app import database
 from app.routes import db
 
 if __name__ == "__main__":
+    db.drop_all()
     db.create_all()
