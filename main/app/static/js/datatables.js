a363af9e771e10088168763d632ef18573b9f92e
BlueEeyore <ronnie.livne@gmail.com>
2025-08-30 18:31:26 +1200

    fixed some css with the datatable on join class page

--- main/app/static/js/datatables.js
+++ main/app/static/js/datatables.js
@@ -1,62 +1,83 @@
-// //initialising join_classes table to have a scrollbar instead of just being as long as possible
-// new DataTable('#join_classes', {
-//     searching: false,
-//     info: false,
-//     paging: false,
-//     scrollCollapse: true,
-//     scrollY: '300px'
-// });
+/*
+  This function formats the content for a child row.
+  It takes the string from the 'data-child-content' attribute.
+*/
+function format(d) {
+    return d;
+}
 
-let table = new DataTable('#join_classes', {
-    columns: [
-        {
-            className: 'dt-control',
-            orderable: false,
-            data: null,
-            defaultContent: ''
-        },
-        { orderable: true },    //name column
-        { orderable: true }     //school column
-    ],
-    order: [[1, 'asc']]
-});
-
-// Add event listener for opening and closing details
-table.on('click', 'tbody td.dt-control', function (e) {
-    let tr = e.target.closest('tr');
-    let row = table.row(tr);
-
-    if (row.child.isShown()) {
-        // This row is already open - close it
-        row.child.hide();
+$(document).ready(function() {
+    
+    // --- Standard Initialization for other tables ---
+    if ($('#classes').length) {
+        var classesTable = $('#classes').DataTable();
     }
-    else {
-        // Get the content from the data- attribute
-        let childContent = $(tr).data('child-content');
-        // Open this row using the content from the attribute
-        row.child(childContent).show();
+    if ($('#lessons').length) {
+        var lessonsTable = $('#lessons').DataTable();
     }
+
+    // --- PROMINENT SEARCH Initialization for the 'join_classes' table ---
+    if ($('#join_classes').length) {
+        
+        // 1. Get references to our custom elements
+        const customSearchInput = $('#custom-search-input');
+
+        // 2. Initialize the DataTable.
+        //    'dom': 'lrtip' is important: it removes the default search box ('f').
+        //    'l'=length, 'r'=processing, 't'=table, 'i'=info, 'p'=pagination
+        var joinTable = $('#join_classes').DataTable({
+            "dom": 'rtp' ,
+            "columnDefs": [
+                {
+                    "orderable": false, // This disables sorting
+                    "targets": 0,      // We are targeting the first column (index 0)
+                }
+            ],
+            "pagingType": "simple" //removes pagination numbers
+        });
+
+        // 3. Attach a keyup event listener to OUR search box.
+        customSearchInput.on('keyup', function() {
+            // 4. On every keypress, apply the search to the DataTable.
+            joinTable.search(this.value).draw();
+        });
+
+        // --- Event listener for the expand/collapse functionality ---
+        $('#join_classes tbody').on('click', 'td.dt-control', function() {
+            var tr = $(this).closest('tr');
+            var row = joinTable.row(tr);
+
+            if (row.child.isShown()) {
+                row.child.hide();
+                tr.removeClass('shown');
+            } else {
+                var childContent = tr.data('child-content');
+                row.child(format(childContent)).show();
+                tr.addClass('shown');
+            }
+        });
+    }
+
+    //initialising classes table to have a scrollbar instead of just being as long as possible
+    new DataTable('#classes', {
+        searching: false,
+        info: false,
+        paging: false,
+        scrollCollapse: true,
+        scrollY: '300px'
+    });
+
+
+    // initialising lessons table to have a scrollbar instead of just being as long as possible
+    new DataTable('#lessons', {
+        searching: false,
+        info: false,
+        paging: false,
+        scrollCollapse: true,
+        scrollY: '300px'
+    });
 });
 
-
-//initialising classes table to have a scrollbar instead of just being as long as possible
-new DataTable('#classes', {
-    searching: false,
-    info: false,
-    paging: false,
-    scrollCollapse: true,
-    scrollY: '300px'
-});
-
-
-// initialising lessons table to have a scrollbar instead of just being as long as possible
-new DataTable('#lessons', {
-    searching: false,
-    info: false,
-    paging: false,
-    scrollCollapse: true,
-    scrollY: '300px'
-});
 
 // // initialising the datatable on search page (with the expandable rows)
 // var table = $("#search").DataTable(
